eb0a8de40ac605fe2b4c385018625099fb6debf9
Brett Jurman <i.be.brett@gmail.com>
2014-08-25 21:57:09 -0400

    finished ex 3

--- ex3/Golf.hs
+++ ex3/Golf.hs
@@ -10,3 +10,8 @@ localMaxima [] = []
 localMaxima l@(a:b:c:xs) 
     | a < b && b > c = [b] ++ (localMaxima $ drop 1 l)
 localMaxima l = localMaxima $ drop 1 l
+
+histogram :: [Integer] -> String
+histSpot x y l = if length  (filter (x==) l) > y then "*" else " "
+maxHist l = maximum [length (filter (x==) l) | x <- [0..9] ] 
+histogram l = foldr (++) [] [ (foldr (++) [] [ histSpot x y l | x <- [0..9] ]) ++ "\n" | y <- reverse [0..(maxHist l) - 1] ] ++ "==========" ++ "\n0123456789\n"
