57af05001f5488b77784f8befe495f6cc9c99da3
Brett Jurman <i.be.brett@gmail.com>
2014-08-27 13:22:23 -0400

    added files needed by ex5

--- ex5/Ex5.hs
+++ ex5/Ex5.hs
@@ -15,3 +15,26 @@ evalStr s =  case (parseExp Lit Add Mul s) of
     Just x@(Add a b) -> Just $ eval x  -- turn this all into one guard
     Just y@(Mul a b) -> Just $ eval y
     Just z@(Lit a)   -> Just $ eval z
+
+class Expr a where
+    add, mul :: a -> a -> a
+    lit :: Integer -> a
+
+instance Expr ExprT where
+    add a b = Add a b
+    mul x y = Mul x y
+    lit n   = Lit n
+
+instance Expr Integer where
+    add a b = a + b
+    mul x y = x * y
+    lit n   = n
+
+instance Expr Bool where
+    add a b = a || b
+    mul x y = x && y
+    lit n
+        | n <= 0 = False
+        | otherwise = True
+
+
