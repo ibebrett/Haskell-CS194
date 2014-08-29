cabc83d43eb78d27a7be364df5ae45a65f139088
Brett Jurman <i.be.brett@gmail.com>
2014-08-28 21:21:13 -0400

    did awesome generating function fib part of ex 6

--- ex6/Fibonacci.hs
+++ ex6/Fibonacci.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 fib :: Integer -> Integer
 fib 0 = 1
 fib 1 = 1
@@ -25,4 +27,27 @@ streamMap f (Stream a rest) = Stream (f a) (streamMap f rest)
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f a = Stream a (streamFromSeed f (f a))
 
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
 
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Stream a arest) (Stream b brest) = (Stream a (Stream b (interleaveStreams arest brest)))
+
+--ruler :: Stream Integer
+
+--x :: Stream Integer
+
+instance Num (Stream Integer) where
+    (+) (Stream a arest) (Stream b brest) = Stream (a+b) (arest + brest)
+    (*) (Stream a arest) (Stream b brest) = (Stream (a*b) (streamRepeat 0)) + (Stream 0  ((streamMap (*a) brest) + (arest*(Stream b brest)) ) )
+    abs (Stream a rest ) = streamMap abs (Stream a rest)
+    signum _ = 1
+    fromInteger i = Stream i (streamRepeat 0)
+    negate (Stream a rest) = streamMap negate (Stream a rest)
+
+instance Fractional (Stream Integer) where
+    (/) (Stream a arest) (Stream b brest) = (Stream (div a b) (streamRepeat 0)) + (Stream 0 ((streamMap (`div` b) (arest - ((Stream a arest)/(Stream b brest))*brest))))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2 ) where
+    x = Stream 0 (Stream 1 (streamRepeat 0))
