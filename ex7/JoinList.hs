869f2509346315772fddcd78ef624a76b6a49359
Brett Jurman <i.be.brett@gmail.com>
2014-09-05 00:00:35 -0400

    added ex2 of ex7

--- ex7/JoinList.hs
+++ ex7/JoinList.hs
@@ -1,6 +1,7 @@
 module JoinList where
 
 import Data.Monoid
+import Sized
 
 data JoinList m a = Empty
                   | Single m a
@@ -14,3 +15,24 @@ tag (Append m _ _) = m
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) a b = Append (tag a <> tag b) a b
 
+indexJ :: (Sized b, Monoid b) => 
+          Int -> JoinList b a -> Maybe a
+
+indexJ index Empty          = Nothing
+indexJ index (Single m a)   = if index == 0 then Just a else Nothing
+indexJ index (Append m a b) = case compare index sizea of
+    LT -> indexJ index a
+    GT -> indexJ (index - sizea) b
+    EQ -> indexJ (index - sizea) b
+    where sizea  = (getSize (size (tag a)))
+
+--dropJ :: (Sized b, Monoid b) =>
+--         Int -> JoinList b a -> JoinList b a
+
+testList :: JoinList Size String
+testList = (Append (Size 2)
+                ( Single (Size 1) "helloworld")
+                ( Single (Size 1) "smile corn")
+           )
+
+testIndex1 = indexJ 1 testList
