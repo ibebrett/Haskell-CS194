5b6ce7129cdf3f71e01ce480687caee204676b46
Brett Jurman <i.be.brett@gmail.com>
2014-09-07 22:24:10 -0400

    99% done with ex8, just have to work on formatting

--- ex8/Party.hs
+++ ex8/Party.hs
@@ -9,7 +9,10 @@ glCons emp (GL emps fun) = GL (emps++[emp]) ((empFun emp) + fun)
 
 instance Monoid GuestList where
     mempty  = (GL [] 0)
-    mappend (GL emps fun) b = (foldr glCons b emps)
+    mappend a (GL emps fun)  = (foldr glCons a emps)
+
+glFun :: GuestList -> Fun
+glFun (GL _ f) = f
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun gla@(GL _ fa) glb@(GL _ fb) = if fa > fb then gla else glb 
@@ -19,6 +22,20 @@ treeVisit t = [rootLabel t] ++ (foldr (++) [] (map (\x -> treeVisit x) (subFores
 
 z = Node 1 [ (Node 2 []), (Node 3 []), (Node 4 []) ]
 
-treeFold :: (b -> a -> b) -> b -> Tree a -> b
-treeFold f s t = foldl f s (treeVisit t)
+treeFold :: (a -> [b] -> b)  -> Tree a -> b
+treeFold f t = f (rootLabel t) (map (treeFold f) (subForest t) )
 
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss [] = (GL [boss] (empFun boss), mempty)
+nextLevel boss prevList = 
+    (
+        foldl (<>) (GL [boss] (empFun boss)) (map snd prevList),  -- if we include the boss, then we have to take the nonboss guys from below
+        foldl (<>) mempty (map (\x -> max (fst x) (snd x)) prevList)
+    )
+
+maxFun :: Tree Employee -> GuestList
+maxFun t = max (fst toplevel) (snd toplevel)
+            where toplevel = treeFold nextLevel t
+
+main :: IO()
+main = readFile "company.txt" >>= (\l -> putStrLn (show (glFun (maxFun (read l )))))
